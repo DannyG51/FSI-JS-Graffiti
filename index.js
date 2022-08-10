3bbf2a5fcb77da68b2c28b33a99d8973c4c3c787
Daniel NONEED <danielhalim01@gmail.com>
2022-08-09 20:05:35 -0700

    added everything

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Select some elements...
+
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius='50px'
+    
+}
+
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let x = 0; x < dogNames.length; x++){
+dogNames[x].style.textAlign = "left"
+}
+
+
+
+let footer = document.querySelector('.footer')
+footer.style.color = "red"
